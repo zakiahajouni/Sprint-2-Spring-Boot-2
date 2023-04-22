ba48e88bc30e034696d98ac1f3a386a851ccabd3
zakiahajouni <zakiahajouni362@gmail.com>
2023-04-22 22:50:28 +0200

    Initial commit

---
+++ src/main/java/com/zakia/Motors/controllors/MotorController.java
@@ -0,0 +1,104 @@
+package com.zakia.Motors.controllors;
+
+import com.zakia.Motors.entities.Motor;
+import com.zakia.Motors.entities.Type;
+import com.zakia.Motors.service.MotorService;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import com.zakia.Motors.service.TypeService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class MotorController {
+    @Autowired
+    MotorService motorService;
+    @Autowired
+    TypeService typeService;
+
+    @RequestMapping("/showCreate")
+    public String showCreate(ModelMap modelMap)
+    {   List<Type> types = typeService.getAllTypes();
+        modelMap.addAttribute("motor", new Motor());
+        modelMap.addAttribute("mode", "new");
+        modelMap.addAttribute("types", types);
+        return "formMotor";
+    }
+    @RequestMapping("/saveMotor")
+    public String saveMotor(@Valid Motor motor,
+                              BindingResult bindingResult)
+    {
+        if (bindingResult.hasErrors()) return "formMotor";
+
+        motorService.saveMotor(motor);
+        return "formMotor";
+    }
+    @RequestMapping("/ListeMotors")
+    public String listeMotors(ModelMap modelMap,
+                                @RequestParam (name="page",defaultValue = "0") int page,
+                                @RequestParam (name="size", defaultValue = "2") int size)
+    {
+        Page<Motor> motos = motorService.getAllMotorsParPage(page, size);
+        modelMap.addAttribute("motors", motos);
+        modelMap.addAttribute("pages", new int[motos.getTotalPages()]);
+        modelMap.addAttribute("currentPage", page);
+        return "listeMotors";
+    }
+
+
+    @RequestMapping("/supprimerMotor")
+    public String supprimerMotor(@RequestParam("id") Long id,
+                                   ModelMap modelMap,
+                                   @RequestParam (name="page",defaultValue = "0") int page,
+                                   @RequestParam (name="size", defaultValue = "2") int size)
+    {
+        motorService.deleteMotorById(id);
+        Page<Motor> motos = motorService.getAllMotorsParPage(page,
+                size);
+        modelMap.addAttribute("motors", motos);
+        modelMap.addAttribute("pages", new int[motos.getTotalPages()]);
+        modelMap.addAttribute("currentPage", page);
+        modelMap.addAttribute("size", size);
+        return "listeMotors";
+    }
+
+
+
+    @RequestMapping("/modifierMotor")
+    public String editerMotor(@RequestParam("id") Long id, ModelMap modelMap) {
+        Motor motor = motorService.getMotor(id);
+        List<Type> types = typeService.getAllTypes();
+        modelMap.addAttribute("motor", motor);
+        modelMap.addAttribute("types", types);
+        modelMap.addAttribute("selectedType", motor.getType());
+        modelMap.addAttribute("mode", "edit");
+        return "formMotor";
+    }
+
+
+    @RequestMapping("/updateMotor")
+    public String updateMotor(@ModelAttribute("motor") Motor motor, @RequestParam("date") String date, ModelMap modelMap) throws ParseException {
+        //conversion de la date
+        SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
+        Date dateCreation = dateformat.parse(String.valueOf(date));
+        motor.setDateCreation(dateCreation);
+
+        motorService.updateMotor(motor);
+        return "redirect:/ListeMotors";
+    }
+
+
+
+
+}
